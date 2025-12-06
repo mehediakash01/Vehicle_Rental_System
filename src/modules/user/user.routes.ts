ae2503feb1103a0bff0870d4b6558fa6a39970de
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-12-06 22:19:45 +0600

    sending user data with success status and message

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -5,7 +5,9 @@ const router = Router();
 router.post("/", async (req: Request, res: Response) => {
   const { name, email, password, phone, role } = req.body;
   if (!password || password.length < 6) {
-    return res.status(400).json({ message: "password must be more than 6 length" });
+    return res
+      .status(400)
+      .json({ message: "password must be more than 6 length" });
   }
 
   const loweredMail = email.toLowerCase();
@@ -16,8 +18,12 @@ router.post("/", async (req: Request, res: Response) => {
       `INSERT INTO users(name,email,password,phone,role) VALUES($1,$2,$3,$4,$5) RETURNING * `,
       [name, loweredMail, hashedPassword, phone, fixedRole]
     );
-    
-    res.send({ message: "Data inserted successfully" });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: result.rows[0],
+    });
   } catch (err: any) {
     res.status(500).json({
       success: false,
