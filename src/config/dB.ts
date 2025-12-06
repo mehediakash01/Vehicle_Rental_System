e2f38a149be91bdf8cdeeeaaf6c039ce8d1c611d
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-12-06 19:46:59 +0600

    seperated the db initialization

---
+++ src/config/dB.ts
@@ -0,0 +1,26 @@
+
+import{ Pool} from "pg";
+import config from ".";
+// Db
+export const pool = new Pool({
+connectionString :`${config.connection_str}`
+})
+
+const initDB  = async()=>{
+    await pool.query(`
+        
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL ,
+        email VARCHAR(100) UNIQUE NOT NULL ,
+        password VARCHAR(255) NOT NULL ,
+        phone INT ,
+        role VARCHAR(50) NOT NULL,
+        createdAt TIMESTAMP DEFAULT NOW(),
+        updatedAt TIMESTAMP DEFAULT NOW()
+        
+        )
+        `)
+
+}
+export default initDB;
