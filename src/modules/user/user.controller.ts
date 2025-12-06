321033a8527c788b8fddd9abe7e96acd1f5e1200
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-12-06 22:26:11 +0600

    created user controller to handle request and response

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { pool } from "../../config/dB";
+// create user
+const createUser = async (req: Request, res: Response) => {
+  const { name, email, password, phone, role } = req.body;
+  if (!password || password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "password must be more than 6 length" });
+  }
+
+  const loweredMail = email.toLowerCase();
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const fixedRole = role === "admin" ? "admin" : "customer";
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name,email,password,phone,role) VALUES($1,$2,$3,$4,$5) RETURNING * `,
+      [name, loweredMail, hashedPassword, phone, fixedRole]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+export const userControllers = {createUser};
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,35 +1,7 @@
 import { Request, Response, Router } from "express";
-import { pool } from "../../config/dB";
-import bcrypt from "bcrypt";
+import { userControllers } from "./user.controller";
+
 const router = Router();
-router.post("/", async (req: Request, res: Response) => {
-  const { name, email, password, phone, role } = req.body;
-  if (!password || password.length < 6) {
-    return res
-      .status(400)
-      .json({ message: "password must be more than 6 length" });
-  }
-
-  const loweredMail = email.toLowerCase();
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const fixedRole = role === "admin" ? "admin" : "customer";
-  try {
-    const result = await pool.query(
-      `INSERT INTO users(name,email,password,phone,role) VALUES($1,$2,$3,$4,$5) RETURNING * `,
-      [name, loweredMail, hashedPassword, phone, fixedRole]
-    );
-
-    res.status(201).json({
-      success: true,
-      message: "User registered successfully",
-      data: result.rows[0],
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
+router.post("/",userControllers.createUser );
 
 export const userRoutes = router;
