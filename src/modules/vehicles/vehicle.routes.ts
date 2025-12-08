2c47827b870f61d4753cbb4ba0e1ed0142cfeb12
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-12-08 20:27:19 +0600

    added vehicle getting route

--- src/modules/vehicles/vehicle.routes.ts
+++ src/modules/vehicles/vehicle.routes.ts
@@ -2,6 +2,9 @@ import { Request, Response, Router } from "express";
 import { vehicleControllers } from "./vehicle.controller";
 
 const router = Router();
+// posting vehicle 
 router.post("/",vehicleControllers.createVehicle)
+// getting all vehicle
+router.get("/",vehicleControllers.gettingVehicle)
 
 export const vehicleRouter = router;
