770442fcc772cdef2a52fa5d8511380bc9c3acd2
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-12-08 18:57:52 +0600

    delteing user:working on userController

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -5,7 +5,7 @@ const router = Router();
 router.post("/",userControllers.createUser );
 router.get("/",userControllers.getUser)
 router.put("/:userId",userControllers.updateUser)
-router.put("/:userId",userControllers.updateUser)
+router.delete("/:userId",userControllers.deleteUser)
 
 export const userRoutes = router;
 
