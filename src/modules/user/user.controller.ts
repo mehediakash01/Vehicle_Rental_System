450964c4488889dd4abbe3df8e392bd92b19dcb5
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-12-08 21:54:47 +0600

    seperate user created logic in auth controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,41 +1,9 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
+
 
 import { userServices } from "./user.service";
 import { pool } from "../../config/dB";
-// create user
-const createUser = async (req: Request, res: Response) => {
-  const { name, email, password, phone, role } = req.body;
-  if (!password || password.length < 6) {
-    return res
-      .status(400)
-      .json({ message: "password must be more than 6 length" });
-  }
 
-  const loweredMail = email.toLowerCase();
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const fixedRole = role === "admin" ? "admin" : "customer";
-  try {
-    const result = await userServices.createUser(
-      name,
-      loweredMail,
-      hashedPassword,
-      phone,
-      fixedRole
-    );
-
-    res.status(201).json({
-      success: true,
-      message: "User registered successfully",
-      data: result.rows[0],
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-};
 // getting all user(only admin)
 const getUser = async (req: Request, res: Response) => {
   try {
@@ -95,4 +63,4 @@ const deleteUser = async (req: Request, res: Response) => {
   }
 };
 
-export const userControllers = { createUser, getUser ,updateUser,deleteUser};
+export const userControllers = {  getUser ,updateUser,deleteUser};
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { userControllers } from "./user.controller";
 
 const router = Router();
-router.post("/",userControllers.createUser );
+// router.post("/",userControllers.createUser );
 router.get("/",userControllers.getUser)
 router.put("/:userId",userControllers.updateUser)
 router.delete("/:userId",userControllers.deleteUser)
