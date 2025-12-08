4aeb6703fa7b04937406e37b5de25e099282e303
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-12-08 19:13:13 +0600

    successfully delete suer

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -71,4 +71,27 @@ const updateUser = async (req: Request, res: Response) => {
   }
 };
 
-export const userControllers = { createUser, getUser ,updateUser};
+// Delete user by only admin and delete only if no bookings is active by the user
+const deleteUser = async (req: Request, res: Response) => {
+
+  const {userId} = req.params;
+
+  try {
+    const result = await userServices.deleteUser(userId)
+    if (!result || result.rowCount===0){
+      return res.status(404).json({success:false,message:"user not found"});
+    }
+   else{
+    res.status(200).json({
+      success:true,
+      message:"User Deleted successfully",
+      data:null
+    })
+
+   } 
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const userControllers = { createUser, getUser ,updateUser,deleteUser};
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,5 @@
 import { pool } from "../../config/dB";
-
+// create user
 const createUser = async(name:string,loweredMail:string,hashedPassword:string,phone:string,fixedRole:string)=>{
      const result = await pool.query(
           `INSERT INTO users(name,email,password,phone,role) VALUES($1,$2,$3,$4,$5) RETURNING * `,
@@ -13,6 +13,7 @@ const getUser = async()=>{
   return result
 
 }
+// update user
 const updateUser = async(name:string,email:string,phone:string,role:string,userId:string|undefined)=>{
   const result = await pool.query(
       `UPDATE users SET name=$1, email=$2, phone=$3, role=$4 WHERE id=$5 RETURNING *`,
@@ -22,8 +23,16 @@ const updateUser = async(name:string,email:string,phone:string,role:string,userI
 
 }
 
+const deleteUser = async(userId:string|undefined)=>{
+ const result =  await pool.query(`DELETE FROM users WHERE id=$1 RETURNING *`,[userId])
+ return result;
+}
+
+
+
 export const userServices = {
     createUser,
     getUser,
-    updateUser
+    updateUser,
+    deleteUser
 }
